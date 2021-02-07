a0cd6a2a1d608c09e3c829a30be82c77130c243c
pranavelric <pranavchoudhary500@gmail.com>
2021-02-07 21:13:51 +0530

    Add images functionality added

--- photoShare/photos/views.py
+++ photoShare/photos/views.py
@@ -1,15 +1,47 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Category, Photo
 # Create your views here.
 
 
 def gallery(request):
-    return render(request, 'photos/gallery.html')
+
+    category = request.GET.get('category')
+
+    if category == None:
+        photos = Photo.objects.all()
+    else:
+        photos = Photo.objects.filter(category__name=category)
+
+    categories = Category.objects.all()
+
+    context = {'categories': categories, 'photos': photos}
+
+    return render(request, 'photos/gallery.html', context)
 
 
 def viewPhoto(request, pk):
-    return render(request, 'photos/photo.html')
+    photo = Photo.objects.get(id=pk)
+    return render(request, 'photos/photo.html', {'photo': photo})
 
 
 def addPhoto(request):
-    return render(request, 'photos/add.html')
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        data = request.POST
+        image = request.FILES.get('image')
+
+        if data['cat'] != 'none':
+            category = Category.objects.get(id=data['cat'])
+        elif data['new_cat'] != '':
+            category, created = Category.objects.get_or_create(
+                name=data['new_cat'])
+        else:
+            category = None
+
+        photo = Photo.objects.create(
+            category=category, desc=data['desc'], image=image)
+
+        return redirect('/')
+
+    context = {'categories': categories}
+    return render(request, 'photos/add.html', context)
